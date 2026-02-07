1ce95cfb3237ea3bf1f201409485ff9fa261ea9e
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-02-07 18:06:27 +0300

    fix(cli): mark console helper functions inline/maybe_unused to silence warnings

--- include/vix/cli/util/Console.hpp
+++ include/vix/cli/util/Console.hpp
@@ -20,35 +20,33 @@ namespace vix::cli::util
 {
   namespace style = vix::cli::style;
 
-  static void log_header_if(bool quiet, const std::string &title)
+  [[maybe_unused]] inline void log_header_if(bool quiet, const std::string &title)
   {
     if (quiet)
       return;
     style::info(title);
   }
 
-  static void log_bullet_if(bool quiet, const std::string &line)
+  [[maybe_unused]] inline void log_bullet_if(bool quiet, const std::string &line)
   {
     if (quiet)
       return;
     style::step(line);
   }
 
-  static void log_hint_if(bool quiet, const std::string &msg)
+  [[maybe_unused]] inline void log_hint_if(bool quiet, const std::string &msg)
   {
     if (quiet)
       return;
     style::hint(msg);
   }
 
-  inline void status_line(
-      bool quiet, const std::string &tag,
-      const std::string &msg)
+  inline void status_line(bool quiet, const std::string &tag, const std::string &msg)
   {
     if (quiet)
       return;
     std::cout << style::PAD << style::BOLD << style::CYAN << tag << style::RESET << " " << msg << "\n";
   }
-} // namespace vix::cli::util
+}
 
 #endif
